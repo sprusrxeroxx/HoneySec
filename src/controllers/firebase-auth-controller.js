3f1367062c76e27c251bfc931566c0b4c88422f0
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-10-16 10:01:29 +0000

    feat: handler function to manager the user log in process

--- src/controllers/firebase-auth-controller.js
+++ src/controllers/firebase-auth-controller.js
@@ -36,3 +36,29 @@ const {
     }
 }
 
+loginUser(req, res) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).json({
+            email: "Email is required",
+            password: "Password is required",
+        });
+    }
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => { 
+          const idToken = userCredential._tokenResponse.idToken
+            if (idToken) {
+                res.cookie('access_token', idToken, {
+                    httpOnly: true
+                });
+                res.status(200).json({ message: "User logged in successfully", userCredential });
+            } else {
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            const errorMessage = error.message || "An error occurred while logging in";
+            res.status(500).json({ error: errorMessage });
+        });
+  }
